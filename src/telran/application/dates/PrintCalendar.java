b75fd34e4db5ab9ad576ed0ce8c34ef256d97fb5
ikrasnova <i.bas@me.com>
2021-12-27 15:38:43 +0200

    hw 24

--- src/telran/application/dates/PrintCalendar.java
+++ src/telran/application/dates/PrintCalendar.java
@@ -9,7 +9,7 @@ import java.util.Locale;
 public class PrintCalendar {
 	private static final TextStyle WEEK_DAY_LENTH = TextStyle.SHORT_STANDALONE;
 	static DayOfWeek[] daysOfWeek = DayOfWeek.values();
-	private static Locale locale = Locale.forLanguageTag("");
+	private static Locale locale = Locale.forLanguageTag("en");
 
 	public static void main(String[] args) {
 		try {
@@ -19,40 +19,66 @@ public class PrintCalendar {
 			// no arguments current month, current year, MONDAY+
 			// no year, no week day - current year, MONDAY+
 			// no week day - MONDAY+
-			setDaysOfWeek(args);
-			if(args.length == 0) {
-			printCalendar(LocalDate.now().getMonthValue(), LocalDate.now().getYear());
+			if (args.length > 2) {
+				updateFirstDaysOfWeek(args[2]);
 			}
-			if(args.length == 1) {
-				printCalendar(Integer.parseInt(args[0]), LocalDate.now().getYear());
-			}
-			if(args.length > 1) 
-			{printCalendar(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
-			}
-		} catch (Exception e) {
+			printCalendar(args);
+		} catch (IllegalArgumentException e) {
 			System.out.println(e.getMessage());
 		}
+	}
 
+	private static void printCalendar(String[] args) throws IllegalArgumentException {
+		int month = LocalDate.now().getMonthValue();
+		int year = LocalDate.now().getYear();
+		String weekDay = "MONDAY";
+		if (args.length != 0) {
+			if (args.length >= 1) {
+				try {
+					month = Integer.parseInt(args[0]);
+				} catch (NumberFormatException e) {
+					System.out.println("wrong month format " + e.getMessage());
+				}
+			}
+			if (args.length > 1) {
+				try {
+					year = Integer.parseInt(args[1]);
+				} catch (NumberFormatException e) {
+					System.out.println("wrong year format " + e.getMessage());
+				}
+			}
+			if (args.length > 2) {
+				weekDay = args[2];
+			}
+		}
+		if (month < 0) {
+			throw new IllegalArgumentException("Month must be greather then zerro");
+		}
+		if (year < 0) {
+			throw new IllegalArgumentException("Year must be greather then zerro");
+		}
+
+		printInputData(month, year, weekDay);
+		printCalendar(month, year);
 
 	}
 
-	private static void setDaysOfWeek(String[] args) throws IllegalArgumentException {
-		if (args.length > 2) {
-			// TODO reordering of static daysOfWeek Done
-			// in case of wrong week day exceptin should be thrown
-			int dayOfWeekToStart = DayOfWeek.valueOf(args[2].toUpperCase()).getValue()-1;
-			LinkedList<DayOfWeek> newOrderForDaysWeek = new LinkedList<>();
-			for( int i = 0; i < daysOfWeek.length; i++) {
-				if (dayOfWeekToStart == daysOfWeek.length) {
-					dayOfWeekToStart = 0;
-				}
-				newOrderForDaysWeek.add(daysOfWeek[dayOfWeekToStart++]);
+	private static void printInputData(int month, int year, String weekDay) {
+		System.out.printf("Input Data are: %d % d %s\n", month, year, weekDay);
+	}
+
+	private static void updateFirstDaysOfWeek(String args) throws IllegalArgumentException {
+		// TODO reordering of static daysOfWeek Done
+		// in case of wrong week day exceptin should be thrown
+		int dayOfWeekToStart = DayOfWeek.valueOf(args.toUpperCase()).getValue() - 1;
+		LinkedList<DayOfWeek> newOrderForDaysWeek = new LinkedList<>();
+		for (int i = 0; i < daysOfWeek.length; i++) {
+			if (dayOfWeekToStart == daysOfWeek.length) {
+				dayOfWeekToStart = 0;
 			}
-			newOrderForDaysWeek.toArray(daysOfWeek);
-		} 
-		else {
-			daysOfWeek = DayOfWeek.values();
+			newOrderForDaysWeek.add(daysOfWeek[dayOfWeekToStart++]);
 		}
+		newOrderForDaysWeek.toArray(daysOfWeek);
 	}
 
 	private static void printCalendar(int month, int year) {
@@ -83,20 +109,20 @@ public class PrintCalendar {
 	}
 
 	private static int getDaysNumbers(int month, int year) {
-		
+
 		return YearMonth.of(year, month).lengthOfMonth();
 	}
 
 	private static void printOffSet(int firstColumn) {
-System.out.print(" ".repeat(firstColumn*getColumnWidth()));		
+		System.out.print(" ".repeat(firstColumn * getColumnWidth()));
 	}
 
 	private static int getFirstColumn(int month, int year) {
 		LocalDate firstDateMonth = LocalDate.of(year, month, 1);
 		int firstWeekDay = firstDateMonth.getDayOfWeek().getValue();
 		int firstValue = daysOfWeek[0].getValue();
-		int delta = firstWeekDay - firstValue ;
-		return  delta >= 0? delta : delta+ daysOfWeek.length ;
+		int delta = firstWeekDay - firstValue;
+		return delta >= 0 ? delta : delta + daysOfWeek.length;
 	}
 
 	private static void printWeekDays() {
